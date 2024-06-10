0babb3657d0677c924e9924f4005737976139ace
LedinN <nick.ledin@hotmail.com>
2024-06-10 10:28:03 +0200

    First commit, add oauth keys

---
+++ src/main/java/dev/nick/itsecprojekt/ItsecprojektApplication.java
@@ -0,0 +1,13 @@
+package dev.nick.itsecprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsecprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsecprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/nick/itsecprojekt/ItsecprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nick.itsecprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsecprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

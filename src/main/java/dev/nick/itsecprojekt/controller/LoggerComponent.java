c92cfbc4e5add60fc53acf3953e1f28540159725
Gustav <gustav.parment1993@gmail.com>
2024-06-11 11:16:03 +0200

    first commit

---
+++ src/main/java/dev/nick/itsecprojekt/controller/LoggerComponent.java
@@ -0,0 +1,14 @@
+package dev.nick.itsecprojekt.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class LoggController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggController.class);
+
+
+}

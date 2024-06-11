8ca063d2107a8ecb4afd5341f8c8159e4251799d
LedinN <nick.ledin@hotmail.com>
2024-06-11 11:30:58 +0200

    delete user from database

--- src/main/java/dev/nick/itsecprojekt/persistence/UserRepository.java
+++ src/main/java/dev/nick/itsecprojekt/persistence/UserRepository.java
@@ -3,6 +3,7 @@ package dev.nick.itsecprojekt.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
 
+
 public interface UserRepository extends JpaRepository<MyUser, Long> {
     MyUser findByEmail(String email);
 }
